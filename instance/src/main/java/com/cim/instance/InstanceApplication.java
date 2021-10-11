ba0d07188bc071af08ff27bfc529d0f473240b97
svalluru <kanth_tetra@yahoo.com>
2021-10-11 17:18:10 -0400

    clean project

---
+++ instance/src/main/java/com/cim/instance/InstanceApplication.java
@@ -0,0 +1,13 @@
+package com.cim.instance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstanceApplication.class, args);
+	}
+
+}
